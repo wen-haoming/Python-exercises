ac18a692f231e1557c4acaec2164321841ede87b
wenhaoming <435203093@qq.com>
2021-02-15 12:48:02 +0800

    test

---
+++ test.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+#  单行注释
+
+'''
+    多行注释
+'''
+
+a = 1
+a = 'sa是df'
+print(a)
+
+# 查看内存地址
+id(3)  # 4488639680
+a = 3
+b = 4
+id(a)  # 4488639680
+
+
+s = 'wenhaoming'
+x = len(s)
+print('the length of %s is %d'%(s,x))
+
+a = [1,2,3,4]
+for b in a:
+    print(b)
+
